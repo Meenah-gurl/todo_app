b6cfc3ca3648d73857c0107e10be49e92436aa4b
meenah-gurl <aminasani.as64@gmail.com>
2023-07-07 10:50:54 +0100

    ....

--- src/Pages/ParallaxEffect.js
+++ src/Pages/ParallaxEffect.js
@@ -14,21 +14,7 @@ const ParallaxEffect = () => {
           <h2 className='text-4xl font-semibold text-center '>Web Development</h2>
         </ParallaxLayer>
 
-        {/* <ParallaxLayer offset={0} speed={0.5}>
-          <img src={url('stars')} style={{ width: '15%', marginLeft: '70%' }} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.3} speed={-0.1}>
-          <img src={url('cloud')} style={{ width: '20%', marginLeft: '55%' }} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.8}>
-          <img src={url('cloud')} style={{ width: '10%', marginLeft: '15%' }} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.75} speed={0.5}>
-          <img src={url('cloud')} style={{ width: '20%', marginLeft: '70%' }} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={2.6} speed={0.4}>
-          <img src={url('cloud')} style={{ width: '20%', marginLeft: '70%' }} />
-        </ParallaxLayer> */}
+       
       </Parallax>
           
     </div>
